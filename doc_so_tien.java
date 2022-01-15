2549bd14fb08d1ee063fbf8cc4c61617331afde1
Trần Hữu Đang <89396036+hdangdev@users.noreply.github.com>
2022-01-15 17:19:33 +0700

    Update doc_so_tien.java

--- doc_so_tien.java
+++ doc_so_tien.java
@@ -11,152 +11,6 @@ import java.util.*;
 public class doc_so_tien {
     private static int i;
 
-    public static void doc_so(char num) {
-        // char num;
-        switch (num) {
-            case '1':
-                System.out.print("Mot ");
-                break;
-            case '2':
-                System.out.print("Hai ");
-                break;
-            case '3':
-                System.out.print("Ba ");
-                break;
-            case '4':
-                System.out.print("Bon ");
-                break;
-            case '5':
-                System.out.print("Nam ");
-                break;
-            case '6':
-                System.out.print("Sau ");
-                break;
-            case '7':
-                System.out.print("Bay ");
-                break;
-            case '8':
-                System.out.print("Tam ");
-                break;
-            case '9':
-                System.out.print("Chin ");
-                break;
-            case '0':
-                System.out.print("Khong ");
-                break;
-        }
-    }
-
-    public static void doc_hang_chuc(String s) {
-        if (s.charAt(0) == '1') {
-            System.out.print("Muoi ");
-            if (s.charAt(1) != '0') {
-                doc_so(s.charAt(1));
-                // doc_so(s.charAt(1));
-            }
-        } else {
-            doc_so(s.charAt(0));
-            System.out.print("Muoi ");
-            if (s.charAt(1) == '5')
-                System.out.print("Lam ");
-            else
-                doc_so(s.charAt(1));
-        }
-    }
-
-    public static void read_money(String s) {
-        String number_temp;
-        if (s.length() == 1) {
-            doc_so(s.charAt(0));
-            System.out.print("nghin dong!");
-        } else if (s.length() == 2) {
-            doc_hang_chuc(s);
-            System.out.print("nghin dong!");
-        } else if (s.length() == 3) {
-            doc_so(s.charAt(0));
-            System.out.print("Tram ");
-            if (s.charAt(1) == '0') {
-                System.out.print("Linh ");
-                doc_so(s.charAt(s.length() - 1));
-            } else {
-                number_temp = s.substring(1);
-                doc_hang_chuc(number_temp);
-            }
-            System.out.print("nghin dong!");
-            // doc_hang_tram(s);
-        } else if (s.length() == 4) {
-            doc_so(s.charAt(0));
-            System.out.print("Trieu ");
-            doc_so(s.charAt(1));
-            System.out.print("Tram ");
-            number_temp = s.substring(2);
-            doc_hang_chuc(number_temp);
-            System.out.print("VND!");
-        } else if (s.length() == 5) {
-            doc_hang_chuc(s);
-            s = s.substring(2);
-            // doc_so(s.charAt(0));
-            System.out.print("Trieu ");
-            doc_so(s.charAt(0));
-            System.out.print("Tram ");
-            number_temp = s.substring(1);
-            doc_hang_chuc(number_temp);
-            System.out.print("VND!");
-        } else if (s.length() == 6) {
-            doc_so(s.charAt(0));
-            System.out.print("Tram ");
-            if (s.charAt(1) == '0') {
-                System.out.print("Linh ");
-                doc_so(s.charAt(2));
-            } else {
-                number_temp = s.substring(1);
-                doc_hang_chuc(number_temp);
-            }
-            System.out.print("Trieu ");
-
-            number_temp = s.substring(1);
-            s = number_temp.substring(2);
-            doc_so(s.charAt(0));
-            System.out.print("Tram ");
-            if (s.charAt(1) == '0') {
-                System.out.print("Linh ");
-                doc_so(s.charAt(s.length() - 1));
-            } else {
-                number_temp = s.substring(1);
-                doc_hang_chuc(number_temp);
-            }
-            System.out.print("VND!");
-        } else if (s.length() == 7) {
-            if (s.charAt(1) > '0' || s.charAt(2) > '0')
-                System.out.print("Tren ");
-            doc_so(s.charAt(0));
-
-            System.out.print("Ty ");
-            System.out.print("VND!");
-        } else {
-            System.out.print("So tien lon hon 99 Ty VND nen khong doc duoc!");
-        }
-        // System.out.print("VND!");
-    }
-
-    public static void dot_money(String s) {
-        // Scanner in = new Scanner(System.in);
-        // System.out.print("Moi ban nhap so N: ");
-        // int n = in.nextInt();
-        // // so N ban dau
-        int cout = 0;
-        // * dinh dang so thap phan
-        // * vd: 12.000.000
-        // */
-        String number = s;
-        for (int i = 0; i < number.length(); i++) {
-            if ((number.length() - i) % 3 == 0 && i != 0) {
-                System.out.print(".");
-            }
-            System.out.print(number.charAt(i));
-        }
-    }
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String s;
@@ -210,6 +64,157 @@ public class doc_so_tien {
         System.out.print(".000 VND");
         System.out.println("");
         read_money(s);
+    }
+    
+    public static void doc_so(char num) {
+        // char num;
+        switch (num) {
+            case '1':
+                System.out.print("Mot ");
+                break;
+            case '2':
+                System.out.print("Hai ");
+                break;
+            case '3':
+                System.out.print("Ba ");
+                break;
+            case '4':
+                System.out.print("Bon ");
+                break;
+            case '5':
+                System.out.print("Nam ");
+                break;
+            case '6':
+                System.out.print("Sau ");
+                break;
+            case '7':
+                System.out.print("Bay ");
+                break;
+            case '8':
+                System.out.print("Tam ");
+                break;
+            case '9':
+                System.out.print("Chin ");
+                break;
+            case '0':
+                System.out.print("Khong ");
+                break;
+        }
+    }
+
+    public static void doc_hang_chuc(String s) {
+        if (s.charAt(0) == '1') {
+            System.out.print("Muoi ");
+            if (s.charAt(1) != '0') {
+                doc_so(s.charAt(1));
+                // doc_so(s.charAt(1));
+            }
+        } else {
+            if (s.charAt(0) == '0') {
+                System.out.print("Linh ");
+                doc_so(s.charAt(1));
+            } else {
+                doc_so(s.charAt(0));
+                System.out.print("Muoi ");
+                if (s.charAt(1) == '5')
+                    System.out.print("Lam ");
+                else
+                    doc_so(s.charAt(1));
+            }
+        }
+    }
+
+    public static void read_money(String s) {
+        String number_temp;
+        if (s.length() == 1) {
+            doc_so(s.charAt(0));
+            System.out.print("nghin dong!");
+        } else if (s.length() == 2) {
+            doc_hang_chuc(s);
+            System.out.print("nghin dong!");
+        } else if (s.length() == 3) {
+            doc_so(s.charAt(0));
+            System.out.print("Tram ");
+            if (s.charAt(1) == '0') {
+                System.out.print("Linh ");
+                doc_so(s.charAt(s.length() - 1));
+            } else {
+                number_temp = s.substring(1);
+                doc_hang_chuc(number_temp);
+            }
+            System.out.print("nghin dong!");
+            // doc_hang_tram(s);
+        } else if (s.length() == 4) {
+            doc_so(s.charAt(0));
+            System.out.print("Trieu ");
+            doc_so(s.charAt(1));
+            System.out.print("Tram ");
+            number_temp = s.substring(2);
+            doc_hang_chuc(number_temp);
+            System.out.print("nghin dong!");
+        } else if (s.length() == 5) {
+            doc_hang_chuc(s);
+            s = s.substring(2);
+            // doc_so(s.charAt(0));
+            System.out.print("Trieu ");
+            doc_so(s.charAt(0));
+            System.out.print("Tram ");
+            number_temp = s.substring(1);
+            doc_hang_chuc(number_temp);
+            System.out.print("nghin dong!");
+        } else if (s.length() == 6) {
+            doc_so(s.charAt(0));
+            System.out.print("Tram ");
+            if (s.charAt(1) == '0') {
+                System.out.print("Linh ");
+                doc_so(s.charAt(2));
+            } else {
+                number_temp = s.substring(1);
+                doc_hang_chuc(number_temp);
+            }
+            System.out.print("Trieu ");
+
+            number_temp = s.substring(1);
+            s = number_temp.substring(2);
+            doc_so(s.charAt(0));
+            System.out.print("Tram ");
+            if (s.charAt(1) == '0') {
+                System.out.print("Linh ");
+                doc_so(s.charAt(s.length() - 1));
+            } else {
+                number_temp = s.substring(1);
+                doc_hang_chuc(number_temp);
+            }
+            System.out.print("nghin dong!");
+        } else if (s.length() == 7) {
+            if (s.charAt(1) > '0' || s.charAt(2) > '0')
+                System.out.print("Tren ");
+            doc_so(s.charAt(0));
+
+            System.out.print("Ty ");
+            System.out.print("VND!");
+        } else {
+            System.out.print("So tien lon hon 99 Ty VND nen khong doc duoc!");
+        }
+        // System.out.print("VND!");
+    }
+
+    public static void dot_money(String s) {
+        // Scanner in = new Scanner(System.in);
+        // System.out.print("Moi ban nhap so N: ");
+        // int n = in.nextInt();
+        // // so N ban dau
+        int cout = 0;
+        // * dinh dang so thap phan
+        // * vd: 12.000.000
+        // */
+        String number = s;
+        for (int i = 0; i < number.length(); i++) {
+            if ((number.length() - i) % 3 == 0 && i != 0) {
+                System.out.print(".");
+            }
+            System.out.print(number.charAt(i));
+        }
     }
 
 }
